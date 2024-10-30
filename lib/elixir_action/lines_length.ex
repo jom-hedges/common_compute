173ec7d6868e643ed4ce47e86b35b94e0147f614
jom-hedges <jom_hedges@proton.me>
2024-10-30 10:41:28 +0800

    added an elixir in action dir

---
+++ lib/elixir_action/lines_length.ex
@@ -0,0 +1,12 @@
+defmodule ElixirAction.LinesLength do
+  @moduledoc"""
+    An exercise from Elixir in Action, p. 110
+
+    A function that returns a list of numbers, with each
+    line representing the length of a corresponding line
+    from the file voyage.txt
+    """
+  def lines_length!(path) do
+    #TODO write function body that pulls in voyage.txt and returns the length of corresponding line.
+  end
+end
